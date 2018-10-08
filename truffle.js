b020630832bbaa95d25278837b3eb3410f079e69
Erik Trautman <erik.trautman@gmail.com>
2018-10-07 17:19:33 -0700

    set up dev server

--- truffle.js
+++ truffle.js
@@ -1,10 +1,10 @@
 /*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
  * function when declaring them. Failure to do so will cause commands to hang. ex:
  * ```
  * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
+ *     provider: function() {
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
  *     },
  *     network_id: '1',
  *     gas: 4500000,
@@ -12,7 +12,15 @@
  *   },
  */
 
-module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-};
+ module.exports = {
+   // See <http://truffleframework.com/docs/advanced/configuration>
+   // to customize your Truffle configuration!
+   // ADDED THIS based on https://hackernoon.com/ethereum-development-walkthrough-part-2-truffle-ganache-geth-and-mist-8d6320e12269
+   networks: {
+     development: {
+       host: "127.0.0.1",
+       port: 7545,
+       network_id: "*" // Match any network id
+     }
+   }
+ };
